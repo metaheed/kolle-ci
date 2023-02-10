f2b5aee8edffea98b815e40916a0b0a0294f8d81
mamun <mamuninfo@gmail.com>
2023-02-10 09:54:41 +0100

    release new version

---
+++ release.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+#git tag v0.0.1 HEAD
+#git push origin --tags
+
+#git checkout -b dev
+#git push --set-upstream origin dev
+
+release_version="v0.0.1"
+
+#release_version="v$(date +'%Y.%m.%d.%H%M%S')"
+
+
+
+echo "################# Check out main branch #######################"
+git checkout main
+git pull
+git commit -m "release new version" -a
+git push
+
+git tag $release_version HEAD
+git push origin --tags
+
